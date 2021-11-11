8aea7cf4f3a296e32f3484de317109c804686c88
Nomoz514 <90491773+Nomoz514@users.noreply.github.com>
2021-11-11 11:28:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1874,7 +1874,7 @@ Game.Launch=function()
 		
 		Game.externalDataLoaded=false;
 		
-		Game.grandmaNames=['Granny','Gusher','Ethel','Edna','Doris','Maud','Hilda','Gladys','Michelle','Michele','Phyllis','Millicent','Muriel','Myrtle','Mildred','Mavis','Helen','Gloria','Sheila','Betty','Gertrude','Agatha','Beryl','Agnes','Pearl','Precious','Ruby','Vera','Bonnie','Ada','Bunny','Cookie','Darling','Gaga','GamGam','Memaw','Mimsy','Peanut','Nana','Nan','Tootsie','Warty','Stinky','Heinous'];
+		Game.grandmaNames=['joe','biden','kamala harris', 'the rock'];
 		Game.customGrandmaNames=[];
 		Game.heralds=0;
 		
